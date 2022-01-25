02bb01980dbdbb34e14823e1bee7ad1c59782c70
Tonio-Lora <anlo@microsoft.com>
2022-01-25 15:20:16 -0600

    Removing quptes from az cli query results

--- configure.sh
+++ configure.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Get IoT Central app ID from previous deployment:
-iotCentralName=$(az deployment group show -n $deploymentName -g $rgName --query properties.outputs.iotCentralName.value)
-iotCentralAppID=$(az iot central app show -n $iotCentralName -g $rgName --query  applicationId)
+iotCentralName=$(az deployment group show -n $deploymentName -g $rgName --query properties.outputs.iotCentralName.value --output tsv)
+iotCentralAppID=$(az iot central app show -n $iotCentralName -g $rgName --query  applicationId --output tsv)
 
 # Get IoT Central Token 
 az account get-access-token --resource https://apps.azureiotcentral.com --only-show-errors
 
 # Deploy Smart Knee Brace imulated devices
-smartKneeBraceDevices=$(az deployment group show -n $deploymentName -g $rgName --query properties.outputs.smartKneeBraceDeviceNumber.value)
+smartKneeBraceDevices=$(az deployment group show -n $deploymentName -g $rgName --query properties.outputs.smartKneeBraceDeviceNumber.value --output tsv)
 echo "3. Creating $smartKneeBraceDevices Smart Knee Brace devices on IoT Central: $iotCentralName ($iotCentralAppID)"
 for (( c=1; c<=$smartKneeBraceDevices; c++ ))
 do
