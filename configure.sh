7119e387c1a46baa245decf3f0c8d07f5d69c292
bwatts64 <32721128+bwatts64@users.noreply.github.com>
2022-01-26 12:26:29 -0500

    Automated Digital Twins Deployment

--- configure.sh
+++ configure.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+# Get Digital Twins instance name
+$dtName=$(az dt list --resource-group $rgName --query [0].name --output tsv)
+
+# Create all the models from folder in git repo
+az dt model create -n $dtName --from-directory ./dtconfig
+az dt twin create -n $dtName --dtmi "dtmi:PatientMonitoring:Facility;1" --twin-id Arkham
+az dt twin create -n $dtName --dtmi "dtmi:PatientMonitoring:Department;1" --twin-id Rehabilitation
+az dt twin create -n $dtName --dtmi "dtmi:PatientMonitoring:Department;1" --twin-id Psychology
+az dt twin relationship create -n $dtName --relationship-id 'contains' --relationship 'facilitycontainsdepartment' --source 'Arkham' --target 'Rehabilitation'
+az dt twin relationship create -n $dtName --relationship-id 'contains' --relationship 'facilitycontainsdepartment' --source 'Arkham' --target 'Psychology'
+
+# Setup array to utilize when assiging devices to departments and patients
+departments=('Rehabilitation' 'Psychology')
+rehapPatients=('Patient1' 'Patient2' 'Patient3')
+psychPatients=('Patient4' 'Patient5' 'Patient6')
+
 # Get IoT Central app ID from previous deployment:
 iotCentralName=$(az deployment group show -n $deploymentName -g $rgName --query properties.outputs.iotCentralName.value --output tsv)
 iotCentralAppID=$(az iot central app show -n $iotCentralName -g $rgName --query  applicationId --output tsv)
@@ -7,22 +23,57 @@ iotCentralAppID=$(az iot central app show -n $iotCentralName -g $rgName --query 
 # Get IoT Central Token 
 az account get-access-token --resource https://apps.azureiotcentral.com --only-show-errors --output none
 
+smartKneeBraceDevices=10
+vitalPatchDevices=10
+
 # Deploy Smart Knee Brace imulated devices
-smartKneeBraceDevices=$(az deployment group show -n $deploymentName -g $rgName --query properties.outputs.smartKneeBraceDeviceNumber.value --output tsv)
+#smartKneeBraceDevices=$(az deployment group show -n $deploymentName -g $rgName --query properties.outputs.smartKneeBraceDeviceNumber.value --output tsv)
 echo "3. Creating $smartKneeBraceDevices Smart Knee Brace devices on IoT Central: $iotCentralName ($iotCentralAppID)"
 for (( c=1; c<=$smartKneeBraceDevices; c++ ))
 do
     deviceId=$(cat /proc/sys/kernel/random/uuid)
-    az iot central device create --device-id $deviceId$c --app-id $iotCentralAppID --template dtmi:j71gm4wvkse:q2hnw2dwt --simulated --only-show-errors --output none
+    #az iot central device create --device-id $deviceId --app-id $iotCentralAppID --template dtmi:j71gm4wvkse:q2hnw2dwt --simulated --only-show-errors --output none
+
+    if ((c%2)); then
+	department=${departments[0]}
+	patient=${rehapPatients[c%3]}
+
+	az dt twin create -n $dtName --dtmi "dtmi:PatientMonitoring:KneeBrace;1" --twin-id $deviceId --properties "{'PatientId': '${patient}'}"
+	echo $deviceId
+	az dt twin relationship create -n $dtName --relationship-id "owns${deviceId}" --relationship 'departmentownsdevice' --source $department --target $deviceId
+    else
+	department=${test[1]}
+	patient=${psychPatients[c%3]}
+
+	az dt twin create -n $dtName --dtmi "dtmi:PatientMonitoring:KneeBrace;1" --twin-id $deviceId --properties "{'PatientId': '${patient}'}"
+	echo $deviceId
+	az dt twin relationship create -n $dtName --relationship-id "owns${deviceId}" --relationship 'departmentownsdevice' --source $department --target $deviceId
+    fi    
 done
  
 # DeployVitals Patch Simulated devices
-vitalPatchDevices=$(az deployment group show -n $deploymentName -g $rgName --query properties.outputs.vitalPatchDevicesNumber.value)
+#vitalPatchDevices=$(az deployment group show -n $deploymentName -g $rgName --query properties.outputs.vitalPatchDevicesNumber.value)
 echo "4. Creating $vitalPatchDevices Vitals Patch devices on IoT Central: $iotCentralName ($iotCentralAppID)"
 for (( c=1; c<=$vitalPatchDevices; c++ ))
 do
     deviceId=$(cat /proc/sys/kernel/random/uuid)
-    az iot central device create --device-id $deviceId$c --app-id $iotCentralAppID --template dtmi:hpzy1kfcbt2:umua7dplmbd --simulated --only-show-errors --output none
+    #az iot central device create --device-id $deviceId --app-id $iotCentralAppID --template dtmi:hpzy1kfcbt2:umua7dplmbd --simulated --only-show-errors --output none
+
+    if ((c%2)); then
+	department=${departments[0]}
+	patient=${rehapPatients[c%3]}
+
+	az dt twin create -n $dtName --dtmi "dtmi:PatientMonitoring:VirtualPatch;1" --twin-id $deviceId --properties "{'PatientId': '${patient}'}"
+	echo $deviceId
+	az dt twin relationship create -n $dtName --relationship-id "owns${deviceId}" --relationship 'departmentownsdevice' --source $department --target $deviceId
+    else
+	department=${test[1]}
+	patient=${psychPatients[c%3]}
+
+	az dt twin create -n $dtName --dtmi "dtmi:PatientMonitoring:VirtualPatch;1" --twin-id $deviceId --properties "{'PatientId': '${patient}'}"
+	echo $deviceId
+	az dt twin relationship create -n $dtName --relationship-id "owns${deviceId}" --relationship 'departmentownsdevice' --source $department --target $deviceId
+    fi 
 done
 
 # On IoT Central, create an Event Hub export destination with json payload
--- modules/digitaltwin.bicep
+++ modules/digitaltwin.bicep
@@ -14,11 +14,11 @@ resource digitaltwin 'Microsoft.DigitalTwins/digitalTwinsInstances@2020-12-01' =
 }
 
 // Get Azure Digital Twins Data Owner role definition
-@description('This is the built-in Azure Digital Twins Data Owner role. See https://docs.microsoft.com/azure/role-based-access-control/built-in-roles')
-resource digitalTwintDataOwnerRoleDefinition 'Microsoft.Authorization/roleDefinitions@2018-01-01-preview' existing = {
-  scope: subscription()
-  name: 'bcd981a7-7f74-457b-83e1-cceb9e632ffe'
-}
+//@description('This is the built-in Azure Digital Twins Data Owner role. See https://docs.microsoft.com/azure/role-based-access-control/built-in-roles')
+//resource digitalTwintDataOwnerRoleDefinition 'Microsoft.Authorization/roleDefinitions@2018-01-01-preview' existing = {
+//  scope: subscription()
+//  name: 'bcd981a7-7f74-457b-83e1-cceb9e632ffe'
+//}
 
 // Grant Azure Digital Twins Data Owner role to user running script
 resource dtroleAssignment 'Microsoft.Authorization/roleAssignments@2020-04-01-preview' = {
